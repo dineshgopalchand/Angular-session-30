abf4789dfab776db9c2073e630821f5217d720a6
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-01-15 14:22:31 +0530

    Created component using cli ng g c <component-name>

--- Angular-session/src/app/course.component.ts
+++ Angular-session/src/app/course.component.ts
@@ -1,14 +1,15 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector:'app-course',
-  template:`
+  selector: 'app-course',
+  template: `
   <div>{{title}}</div>
+  <app-courses></app-courses>
   `
 })
-export class CourseComponent{
-  title='Course component!!';
-  constructor(){
+export class CourseComponent {
+  title = 'Course component!!';
+  constructor() {
     console.log('course component');
   }
 
