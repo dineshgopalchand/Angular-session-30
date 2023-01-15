9b70cbda3f4985b8ce158879e432c7c9c099570f
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-01-15 13:31:59 +0530

    constructer shorthand feature

--- ts/classes.js
+++ ts/classes.js
@@ -5,37 +5,31 @@ class Student {
     //     this.studentName = name;
     //     this.location = location;
     // }
-    constructor(private studentName: string, private location: string) {
+    constructor(studentName, location) {
+        this.studentName = studentName;
+        this.location = location;
     }
-
-    protected showDetails() {
+    showDetails() {
         return { name: this.studentName, location: this.location };
     }
     showFormattedDetails() {
-        console.log('Student details :', this.showDetails())
+        console.log('Student details :', this.showDetails());
     }
 }
-
 const studentObj = new Student('dinesh', 'BRL');
 const studentObj1 = new Student('Gopal', 'BBSR');
 console.log(studentObj.showFormattedDetails());
 console.log(studentObj1.showFormattedDetails());
 // studentObj1.location='DHN';
 console.log(studentObj1.showFormattedDetails());
-
 class MedicalStudent extends Student {
-    constructor(name: string, location: string, private collageName: string) {
+    constructor(name, location, collageName) {
         super(name, location);
-
+        this.collageName = collageName;
     }
     showAllDetails() {
-        return { ...this.showDetails(), collage: this.collageName }
+        return Object.assign(Object.assign({}, this.showDetails()), { collage: this.collageName });
     }
 }
-
 const mstudent1 = new MedicalStudent('runa', 'BBSR', 'sumHospital');
-
-console.log(mstudent1.showAllDetails())
-
-
-
+console.log(mstudent1.showAllDetails());
