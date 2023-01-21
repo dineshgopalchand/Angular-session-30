8724163113284bdbdb194f42b171358f87928b3f
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-01-21 11:44:04 +0530

    Discussed Component

--- Angular-session/src/app/course.component.ts
+++ Angular-session/src/app/course.component.ts
@@ -3,14 +3,19 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-course',
   template: `
-  <div>{{title}}</div>
+  <div>{{showTitle()}}</div>
+  <p>{{1+1+1}}</p>
   <app-courses></app-courses>
   `
 })
 export class CourseComponent {
-  title = 'Course component!!';
+  private title = 'Course component!!';
   constructor() {
     console.log('course component');
+  }
+
+  showTitle() {
+    return this.title.toUpperCase();
   }
 
 }
