56b1a33c7cd82ed332561bac23d7c677e7dc1474
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-02-12 12:43:02 +0530

    Directive - NgIf else then discussed

--- Angular-session/src/app/common/todo-list/todo-list.component.ts
+++ Angular-session/src/app/common/todo-list/todo-list.component.ts
@@ -3,13 +3,48 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
+  todoList: TodoItem[] = [];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  addTodo(todoinput: HTMLInputElement) {
+    const newTodo = todoinput.value.trim();
+    if (newTodo.length > 3) {
+      this.todoList.push({
+        id: Date.now(),
+        active: true,
+        title: newTodo,
+      });
+      todoinput.value = '';
+    }
   }
-
+  deleteCourse(item: TodoItem) {
+    this.todoList = this.todoList.filter((todo) => todo.id != item.id);
+  }
+  updateStatus(item: TodoItem) {
+    this.todoList = this.todoList.map((todo) => {
+      return {
+        ...todo,
+        active: todo.id == item.id ? !todo.active : todo.active,
+      };
+    });
+  }
+}
+// export type todoItem={
+//   id:number;
+//   title:string;
+//   active:boolean;
+// }
+// export class TodoItem{
+//   id!:number;
+//   title!:string;
+//   active!:boolean;
+// }
+export interface TodoItem {
+  id: number;
+  title: string;
+  active: boolean;
 }
