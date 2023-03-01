1fa6979cd44c90804d288908331c81a042332c37
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-03-01 22:40:41 +0530

    Discussed ngModelGroup

--- Angular-session/src/app/contact/contact.component.ts
+++ Angular-session/src/app/contact/contact.component.ts
@@ -12,6 +12,7 @@ export class ContactComponent implements OnInit {
 
   ngOnInit(): void {}
   formSubmit(form: NgForm) {
+    console.log(form.value);
     if (form.valid) {
       console.log(form.value);
       // call api to send form details
