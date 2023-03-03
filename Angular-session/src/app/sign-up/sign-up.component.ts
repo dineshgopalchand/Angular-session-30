645cef4d7de63eeebe0f3709187174e92d978078
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-03-03 21:21:13 +0530

    Discussed basic of FormGroup and FormControl

--- Angular-session/src/app/sign-up/sign-up.component.ts
+++ Angular-session/src/app/sign-up/sign-up.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  signUpForm: FormGroup;
+  constructor() {
+    this.signUpForm=new FormGroup({
+      'fullname': new FormControl(''),
+      'email': new FormControl(''),
+      'password': new FormControl(''),
+      'repass': new FormControl(''),
+    });
   }
 
+  ngOnInit(): void {
+    console.log(this.signUpForm);
+  }
+  signUpSubmit(){
+    console.log(this.signUpForm.value);
+    this.signUpForm.reset()
+  }
 }
--- Angular-session/src/app/sign-up/sign-up.module.ts
+++ Angular-session/src/app/sign-up/sign-up.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignUpComponent } from './sign-up.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [SignUpComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [SignUpComponent],
 })
 export class SignUpModule {}
