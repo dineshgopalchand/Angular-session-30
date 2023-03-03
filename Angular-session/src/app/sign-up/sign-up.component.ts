6a604f489101867c8259b7dcb3e9184598842fef
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-03-03 21:43:21 +0530

    Added basic validation in signup form(reactive form)

--- Angular-session/src/app/sign-up/sign-up.component.ts
+++ Angular-session/src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -9,19 +9,35 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class SignUpComponent implements OnInit {
   signUpForm: FormGroup;
   constructor() {
-    this.signUpForm=new FormGroup({
-      'fullname': new FormControl(''),
-      'email': new FormControl(''),
-      'password': new FormControl(''),
-      'repass': new FormControl(''),
+    this.signUpForm = new FormGroup({
+      fullname: new FormControl('', Validators.required),
+      email: new FormControl('dinesh@gmail.com', [
+        Validators.required,
+        Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/),
+      ]),
+      password: new FormControl('pass123', [Validators.required]),
+      repass: new FormControl('pass123', [Validators.required]),
     });
   }
 
   ngOnInit(): void {
     console.log(this.signUpForm);
   }
-  signUpSubmit(){
+  get fullname(): FormControl {
+    return this.signUpForm.get('fullname') as FormControl;
+  }
+  get email(): FormControl {
+    return this.signUpForm.get('email') as FormControl;
+  }
+  get password(): FormControl {
+    return this.signUpForm.get('password') as FormControl;
+  }
+  get repass(): FormControl {
+    return this.signUpForm.get('repass') as FormControl;
+  }
+  signUpSubmit() {
     console.log(this.signUpForm.value);
-    this.signUpForm.reset()
+    console.log(this.signUpForm.get('fullname'));
+    // this.signUpForm.reset();
   }
 }
